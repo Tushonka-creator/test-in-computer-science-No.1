70010747e362988b731a49588578148855d8ed19
Ivan Tushin <ivantushin12@mail.ru>
2025-09-27 15:21:40 +0700

    Ex.1 add

---
+++ Ex.1/main.py
@@ -0,0 +1,11 @@
+input_bits = 1000
+bytes = input_bits/8
+print(bytes)
+Kbytes = bytes/1024
+print(Kbytes)
+Mbytes = Kbytes/1024
+print(Mbytes)
+Gbytes = Mbytes/1024
+print(Gbytes)
+Tbytes = Gbytes/1024
+print(Gbytes)
